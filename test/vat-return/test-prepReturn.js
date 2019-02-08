61cfd6caaa44ec743acbce67fce498f8af0a244f
richard <richardmilton@ahotclub.com>
2019-02-08 08:27:41 +0000

    refactor db queries

--- test/vat-return/test-prepReturn.js
+++ test/vat-return/test-prepReturn.js
@@ -2,10 +2,14 @@ const expect = require('chai').expect;
 const prepReturn = require('../../vat-return/prepReturn');
 
 describe('vat-return.prepReturn()', function () {
-  it('should not error', function () {
+  it('should return JSON containing 3 objects contianing 11, 6 and 7 ', async function () {
 
-    let res = prepReturn();
-    res.then(data => console.log(data))
+    let data = await prepReturn();
+
+    console.log(data);
+    expect(Object.keys(data.submitted).length).to.be.equal(11);
+    expect(Object.keys(data.due).length).to.be.equal(6);
+    expect(Object.keys(data.declare).length).to.be.equal(7);
 
   });
 });
--- vat-return/prepReturn.js
+++ vat-return/prepReturn.js
@@ -4,7 +4,8 @@ const dateFormat = require('dateformat');
 const getTrialReturn = (month, year) => {
 
   return new Promise(async (resolve, reject) => {
-    let now = dateFormat(Date.now(), 'yyyy-mm-dd');
+    let now = Date.now();
+    const timeIt = (startTime) => console.log(Date.now() - startTime);
 
     let totalSubmittedSql = "SELECT " +
       "month(date_add(max(EndDate), interval 3 month)) as NextEndMonth, " +
@@ -16,30 +17,50 @@ const getTrialReturn = (month, year) => {
       "sum(InputVAT) as InputVAT, sum(OutputVAT) as OutputVAT, " +
       "sum(ECPurchases) as ECPurchases, sum(ECSales) as ECSales " +
       "FROM vathistory where Status='Actual'";
-
     let totalSubmitted = await data.sqPl.query(totalSubmittedSql, { type: data.Sequelize.QueryTypes.SELECT});
+    timeIt(now);
 
     let qEnd = `${year || totalSubmitted[0].NextEndYear}-${month || totalSubmitted[0].NextEndMonth}-31`;
 
-    let sql = "SELECT sum((PurchaseAmount/100)-(PurchaseAmount/100/(1+(VatRate/10000)))) as 'value' FROM ordertable o where VatRate>0 and InvoiceDate <= ?";
+    let sql = "SELECT " +
+      "sum((PurchaseAmount/100)-(PurchaseAmount/100/(1+(VatRate/10000)))) as 'value' " +
+      "FROM ordertable o where InvoiceDate <= ? and VatRate>0";
     let totalOutputVATToDate = await data.sqAdmin.query(sql, {replacements: [qEnd], type: data.Sequelize.QueryTypes.SELECT});
+    timeIt(now);
 
-    sql = "SELECT round(sum(NetAmount/100*TaxRate/100))/100 as inputVAT FROM invoice_item j inner join invoice i on i.InvoiceId=j.InvoiceId where InvoiceDate <= ?";
+    sql = "SELECT " +
+      "round(sum(NetAmount/100*TaxRate/100))/100 as inputVAT " +
+      "FROM invoice_item j inner join invoice i on i.InvoiceId=j.InvoiceId where InvoiceDate <= ?";
     let totalInputVATToDate = await data.sqPl.query(sql, {replacements: [qEnd],  type: data.Sequelize.QueryTypes.SELECT});
+    timeIt(now);
 
-    sql = "SELECT if(sum(if(PurchaseCurrency=978,round(PurchaseAmount*Rate),PurchaseAmount)/100) is null, 0, sum(if(PurchaseCurrency=978,round(PurchaseAmount*Rate),PurchaseAmount)/100)) as total FROM ordertable o inner join exchange_rates on month(MonthStart)=month(InvoiceDate) and year(MonthStart)=year(InvoiceDate) where CpiResultsCode in (0,100)" +
-      " and InvoiceDate >= '2007-01-01' and InvoiceDate <= ?";
+    sql = "SELECT " +
+      "if(sum(if(PurchaseCurrency=978,round(PurchaseAmount*Rate),PurchaseAmount)/100) is null, 0, sum(if(PurchaseCurrency=978,round(PurchaseAmount*Rate),PurchaseAmount)/100)) as total " +
+      "FROM ordertable o inner join exchange_rates on month(MonthStart)=month(InvoiceDate) and year(MonthStart)=year(InvoiceDate) " +
+      "where CpiResultsCode in (0,100) and InvoiceDate >= '2007-01-01' and InvoiceDate <= ?";
     let netSalesToDate = await data.sqAdmin.query(sql, {replacements: [qEnd], type: data.Sequelize.QueryTypes.SELECT});
+    timeIt(now);
 
-    sql = "SELECT if(sum(if(CurrencyId=978,(round(NetAmount*Rate*10)/1000),NetAmount/100)) is null, 0, sum(if(CurrencyId=978,(round(NetAmount*Rate*10)/1000),NetAmount/100))) as total FROM invoice j inner join exchange_rates on month(MonthStart)=month(InvoiceDate) and year(MonthStart)=year(InvoiceDate) inner join invoice_item i on i.InvoiceId=j.InvoiceId" +
-      " where InvoiceDate <= ?";
+    sql = "SELECT " +
+      "if(sum(if(CurrencyId=978,(round(NetAmount*Rate*10)/1000),NetAmount/100)) is null, 0, sum(if(CurrencyId=978,(round(NetAmount*Rate*10)/1000),NetAmount/100))) as total " +
+      "FROM invoice j inner join exchange_rates on month(MonthStart)=month(InvoiceDate) and year(MonthStart)=year(InvoiceDate) " +
+      "inner join invoice_item i on i.InvoiceId=j.InvoiceId where InvoiceDate <= ?";
     let netPurchasesToDate = await data.sqPl.query(sql, {replacements: [qEnd], type: data.Sequelize.QueryTypes.SELECT});
+    timeIt(now);
 
-    sql = "SELECT if(sum(if(j.CurrencyId=978,(round(if(NetAmount*Rate is null,0,NetAmount*Rate)*10)/1000),NetAmount/100)) is null,0,sum(if(j.CurrencyId=978,(round(if(NetAmount*Rate is null,0,NetAmount*Rate)*10)/1000),NetAmount/100))) as ecPurchases FROM invoice_item i inner join invoice j on i.InvoiceId=j.InvoiceId inner join exchange_rates e on month(MonthStart)=month(InvoiceDate) and year(MonthStart)=year(InvoiceDate) inner join organisation o on o.OrganisationId=j.SupplierId where CountryCode<>826 and InvoiceDate <= ?";
+    sql = "SELECT if(sum(if(j.CurrencyId=978,(round(if(NetAmount*Rate is null,0,NetAmount*Rate)*10)/1000),NetAmount/100)) is null,0,sum(if(j.CurrencyId=978,(round(if(NetAmount*Rate is null,0,NetAmount*Rate)*10)/1000),NetAmount/100))) as ecPurchases " +
+      "FROM invoice_item i inner join invoice j on i.InvoiceId=j.InvoiceId " +
+      "inner join exchange_rates e on month(MonthStart)=month(InvoiceDate) and year(MonthStart)=year(InvoiceDate) " +
+      "inner join organisation o on o.OrganisationId=j.SupplierId where CountryCode<>826 and InvoiceDate <= ?";
     let totalECPurchasesToDate = await data.sqPl.query(sql, {replacements: [qEnd], type: data.Sequelize.QueryTypes.SELECT});
+    timeIt(now);
 
-    sql = "SELECT sum(if(PurchaseCurrency=978,round(PurchaseAmount*Rate),PurchaseAmount)/100) as ecSales FROM ordertable o inner join exchange_rates on month(MonthStart)=month(InvoiceDate) and year(MonthStart)=year(InvoiceDate) inner join retailers r on Retailer_id=o.RetailerId inner join address a on r.Retailer_Id=a.RetailerId where CpiResultsCode in (0,100) and Country<>'826' and InvoiceDate >= '2007-01-01' and InvoiceDate <= ?";
-    let totlaECSalesToDate = await data.sqAdmin.query(sql, {replacements: [qEnd], type: data.Sequelize.QueryTypes.SELECT});
+    sql = "SELECT sum(if(PurchaseCurrency=978,round(PurchaseAmount*Rate),PurchaseAmount)/100) as ecSales " +
+      "FROM ordertable o inner join retailers r on Retailer_id=o.RetailerId " +
+      "inner join exchange_rates on month(MonthStart)=month(InvoiceDate) and year(MonthStart)=year(InvoiceDate) " +
+      "inner join address a on r.Retailer_Id=a.RetailerId where CpiResultsCode in (0,100) and InvoiceDate >= '2007-01-01' and InvoiceDate <= ? and Country<>'826'";
+    let totalECSalesToDate = await data.sqAdmin.query(sql, {replacements: [qEnd], type: data.Sequelize.QueryTypes.SELECT});
+    timeIt(now);
 
     let trialJson = {
       //grossSales: gross,
@@ -50,7 +71,7 @@ const getTrialReturn = (month, year) => {
         InputVAT: totalInputVATToDate[0].inputVAT,
         OutputVAT: totalOutputVATToDate[0].value,
         ECPurchases: totalECPurchasesToDate[0].ecPurchases,
-        ECSales: totlaECSalesToDate[0].ecSales
+        ECSales: totalECSalesToDate[0].ecSales
       },
       declare: {
         Inputs: Math.round(netPurchasesToDate[0].total - totalSubmitted[0].InputTotals),
@@ -58,10 +79,12 @@ const getTrialReturn = (month, year) => {
         InputVAT: (totalInputVATToDate[0].inputVAT - totalSubmitted[0].InputVAT).toFixed(2),
         OutputVAT: (totalOutputVATToDate[0].value - totalSubmitted[0].OutputVAT).toFixed(2),
         VATPayable: ((totalOutputVATToDate[0].value - totalSubmitted[0].OutputVAT) - (totalInputVATToDate[0].inputVAT - totalSubmitted[0].InputVAT)).toFixed(2),
-        ECPurchases: Math.round(totalECPurchasesToDate[0].ecPurchases - totalSubmitted[0].ECPurchases),
-        ECSales: Math.round(totlaECSalesToDate[0].ecSales - totalSubmitted[0].ECSales)
+        ECPurchases: Math.round(totalECPurchasesToDate[0].ecPurchases - totalSubmitted[0].ECPurchases) || 0,
+        ECSales: Math.round(totalECSalesToDate[0].ecSales - totalSubmitted[0].ECSales) || 0
       }
     }
+
+    console.log(Date.now() - now);
 
     resolve(trialJson);
 
