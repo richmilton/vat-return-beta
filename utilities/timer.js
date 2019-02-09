cb8439025d70eb601c38ca4817bb3ca9b187107d
richard <richardmilton@ahotclub.com>
2019-02-09 10:11:02 +0000

    abstraction of timer to utilities

--- utilities/timer.js
+++ utilities/timer.js
@@ -1,3 +1,3 @@
 require('dotenv').config();
 
-module.exports = (startTime) => {if (process.env.TIMER) console.log(Date.now() - startTime)};
+module.exports = (startTime) => {if (/^true$|^yes$/.test(process.env.TIMER)) console.log(Date.now() - startTime)};
