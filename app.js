606abe29aaf8bec9d0950a1a8a120b284345c97c
richard <richardmilton@ahotclub.com>
2019-02-11 15:56:35 +0000

    rough (and I mean rough) working retrieval of test account obligations

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
-//const simpleOauthModule = require('simple-oauth2');
-//const request = require('superagent');
-const express = require('express');
 
+const express = require('express');
 const winston = require('winston');
 const log = winston.createLogger({
   transports: [
@@ -10,13 +8,21 @@ const log = winston.createLogger({
   ]
 });
 
-//const serverToken = '74c91e7bbdd7d8ce70fd111968c4b72b';
+const serviceVersion = '1.0';
+
+//TODO - upgrade to latest simple-oauth2
 const simpleOauthModule = require('simple-oauth2');
+
 const appPort = process.env.APP_PORT;
 const oauthScope = 'read:vat write:vat';
 
 const cookieSession = require('cookie-session');
-const redirectUri = `http://localhost:${appPort}/`;
+const redirectUri = `http://localhost:${appPort}/auth/callback`;
+const userRestrictedEndpoint = '/666679046/obligations?from=2018-01-01&to=2018-12-31'
+const apiBaseUrl = process.env.HMRC_API_BASE_URL;
+const serviceName = 'organisations/vat';
+const superAgent = require('superagent');
+const prepReturn = require('./vat-return/prepReturn');
 
 const oauth2 = simpleOauthModule.create({
   client: {
@@ -24,7 +30,7 @@ const oauth2 = simpleOauthModule.create({
     secret: process.env.HMRC_API_CLIENT_SECRET,
   },
   auth: {
-    tokenHost: process.env.HMRC_API_BASE_URL,
+    tokenHost: apiBaseUrl,
     tokenPath: '/oauth/token',
     authorizePath: '/oauth/authorize',
   },
@@ -48,17 +54,17 @@ app.use(cookieSession({
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  console.log(req.session);
+  log.debug(req.session);
 
   //get obligations
 
   res.render('index', {
     loggedIn: req.session.oauth2Token || false,
-
+    data: req.session['data']
   });
 });
 
-app.get("/userCall",(req,res) => {
+app.get("/login",(req,res) => {
   if(req.session.oauth2Token){
     var accessToken = oauth2.accessToken.create(req.session.oauth2Token);
 
@@ -68,7 +74,7 @@ app.get("/userCall",(req,res) => {
         .then((result) => {
           log.info('Refreshed token: ', result.token);
           req.session.oauth2Token = result.token;
-          callApi(userRestrictedEndpoint, res, result.token.access_token);
+          callApi(userRestrictedEndpoint, res, result.token.access_token, req, '/');
         })
         .catch((error) => {
           log.error('Error refreshing token: ', error);
@@ -76,42 +82,82 @@ app.get("/userCall",(req,res) => {
         });
     } else {
       log.info('Using token from session: ', accessToken.token);
-      callApi(userRestrictedEndpoint, res, accessToken.token.access_token);
+      callApi(userRestrictedEndpoint, res, accessToken.token.access_token, req, '/');
     }
   } else {
     log.info('Need to request token')
-    req.session.caller = '/userCall';
+    req.session.caller = '/login';
     res.redirect(authorizationUri);
   }
 });
 
+app.get('/auth/callback', (req, res) => {
+  const options = {
+    redirect_uri: redirectUri,
+    code: req.query.code
+  };
+  log.info(options);
+
+  oauth2.authorizationCode.getToken(options, (error, result) => {
+    if (error) {
+      log.error('Access Token Error: ', error);
+      return res.json('Authentication failed');
+    }
+
+    log.info('Got token: ', result);
+    // save token on session and return to calling page
+    req.session.oauth2Token = result;
+    res.redirect('/login');
+  });
+});
+
 app.get("/logout", (req, res) => {
-  req.session.oauth2Token = null;
+  req.session = null;
   res.redirect('/');
 });
 
-const callApi = (resource, res, bearerToken) => {
+app.get("/prepReturn", async (req, res) => {
+  let returnData = await prepReturn();
+  if (req.session.data) {
+    req.session.data['returnData'] = returnData;
+  }
+  res.redirect('/');
+});
+
+
+const callApi = (resource, res, bearerToken, req, redir) => {
   const acceptHeader = `application/vnd.hmrc.${serviceVersion}+json`;
   const url = apiBaseUrl + serviceName + resource;
   log.info(`Calling ${url} with Accept: ${acceptHeader}`);
-  const req = request
+  const sReq = superAgent
     .get(url)
     .accept(acceptHeader);
 
   if(bearerToken) {
     log.info('Using bearer token:', bearerToken);
-    req.set('Authorization', `Bearer ${bearerToken}`);
+    sReq.set('Authorization', `Bearer ${bearerToken}`);
   }
 
-  req.end((err, apiResponse) => handleResponse(res, err, apiResponse));
+  sReq.end((err, apiResponse) => handleResponse(res, err, apiResponse, req, redir));
 };
 
-const handleResponse = (res, err, apiResponse) => {
+const handleResponse = (res, err, apiResponse, req, redir) => {
   if (err || !apiResponse.ok) {
     log.error('Handling error response: ', err);
     res.send(err);
   } else {
-    res.send(apiResponse.body);
+    dataItem = apiResponse.body;
+    for (let key in dataItem) {
+      req.session.data = {};
+      req.session.data[key] = dataItem[key];
+    }
+
+    if (redir) {
+      res.redirect('/');
+    }
+    else {
+      res.send(apiResponse.body);
+    }
   }
 };
 
